1abb47ca7f68e2cbd8ca969413389f470b901bee
frogcat <frogcat@users.noreply.github.com>
2016-04-21 03:39:16 +0900

    Add css

--- leaflet-control-fader.js
+++ leaflet-control-fader.js
@@ -2,69 +2,82 @@
 
   L.Control.Fader = L.Control.extend({
     options: {
-      position: 'bottomleft'
+      position: 'bottomleft',
+      orientation: 'horizontal',
+      initialRatio: 0.5
     },
-    initialize: function(layers) {
-      this._layers = layers;
-      this._ratio = 0.5;
+    initialize: function(layers, options) {
+      L.Util.setOptions(this, options);
+      this._layers = [];
+      this._clazz = "leaflet-control-fader-" + this.options.orientation;
+      layers.forEach(function(layer) {
+        if (layer.getContainer) {
+          L.DomUtil.addClass(layer.getContainer(), this._clazz);
+          this._layers.push(layer);
+        }
+      }, this);
+      this._ratio = this.options.initialRatio;
+    },
+    _initContainer: function() {
+      var container = L.DomUtil.create('div', this._clazz);
+      if (this.options.orientation == "vertical")
+        container.innerHTML = "&#x25B2;<br/>&#x25BC;";
+      else
+        container.innerHTML = "&#x25C4;&#x25BA;";
+
+      var draggable = new L.Draggable(container);
+      draggable.enable();
+      draggable.on('drag', this._onDrag, this);
+      this._container = container;
+    },
+    _onDrag: function() {
+      var size = this._map.getSize();
+      var position = L.DomUtil.getPosition(this._container);
+      if (this.options.orientation == "vertical")
+        this._ratio = Math.min(1, Math.max(0, position.y / size.y));
+      else
+        this._ratio = Math.min(1, Math.max(0, position.x / size.x));
+      this._update();
     },
     onAdd: function(map) {
-      var sz = this._map.getSize();
-      var ll = map.containerPointToLatLng([sz.x * this._ratio, sz.y]);
+      this._initContainer();
       this._map = map;
-      this._map.on("move", this._update, this);
-
-      var e = document.createElement('div');
-      e.innerHTML = "&#9668;&#9658;";
-      var css = "text-shadow: 1px 0px white, 0px 1px white, -1px 0px white, 0px -1px white;";
-      css += "text-align:center;";
-      css += "font-size:28px;";
-      css += "cursor:pointer;";
-      css += "color:#0078A8;";
-      css += "margin-left:-1em;";
-      css += "width:2em;";
-      e.setAttribute("style", css);
-      this._container = e;
-      var draggable = new L.Draggable(e);
-      draggable.enable();
-      draggable.on("drag", function(event) {
-        this._ratio = L.DomUtil.getPosition(e).x / this._map.getSize().x;
-        this._ratio = Math.min(1, Math.max(0, this._ratio));
-        this._update();
-      }, this);
+      this._map.on('move', this._update, this);
       this._update();
       return this._container;
     },
     onRemove: function() {
-      this._map.off("move", this._update, this);
+      this._map.off('move', this._update, this);
     },
     _update: function() {
-      var sz = this._map.getSize();
-      var cb = L.bounds([0, 0], [sz.x * this._ratio, sz.y]);
-      var lb = L.bounds(
-        this._map.containerPointToLayerPoint(cb.min),
-        this._map.containerPointToLayerPoint(cb.max)
-      );
-      L.DomUtil.setPosition(this._container, L.point(sz.x * this._ratio, 0));
+
+      var outer = this._map.getSize();
+      var inner = outer.multiplyBy(this._ratio);
+      var min = this._map.containerPointToLayerPoint([0, 0]);
+      var max = this._map.containerPointToLayerPoint([inner.x, outer.y]);
+      if (this.options.orientation == "vertical") {
+        max = this._map.containerPointToLayerPoint([outer.x, inner.y]);
+        L.DomUtil.setPosition(this._container, L.point(0, inner.y));
+      } else {
+        L.DomUtil.setPosition(this._container, L.point(inner.x, 0));
+      }
+
       this._layers.forEach(function(layer) {
         var e = layer.getContainer();
-        e.style["overflow"] = "hidden";
-        e.style["background"] = "rgba(0,0,0,0.25)";
-        e.style["border-right"] = "1px solid #0078A8";
-        e.style["left"] = lb.min.x + "px";
-        e.style["top"] = lb.min.y + "px";
-        e.style["width"] = lb.getSize().x + "px";
-        e.style["height"] = lb.getSize().y + "px";
+        e.style.left = min.x + "px";
+        e.style.top = min.y + "px";
+        e.style.width = (max.x - min.x) + "px";
+        e.style.height = (max.y - min.y) + "px";
         for (var f = e.firstChild; f; f = f.nextSibling) {
           if (f.style) {
-            f.style["margin-top"] = (-lb.min.y) + "px";
-            f.style["margin-left"] = (-lb.min.x) + "px";
+            f.style.marginTop = (-min.y) + "px";
+            f.style.marginLeft = (-min.x) + "px";
           }
         }
       });
     }
   });
-  L.control.fader = function(layers) {
-    return new L.Control.Fader(layers);
+  L.control.fader = function(layers, options) {
+    return new L.Control.Fader(layers, options);
   };
 })(window);
